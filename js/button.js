07cb3ea2a398413b891ec2877e8565d400e29aeb
aj300 <sam300542@163.com>
2025-09-27 15:31:26 +0800

    update
    宋高宗赐岳飞手敕

--- js/button.js
+++ js/button.js
@@ -32,8 +32,10 @@ const buttonMap = {
     indexS: "indexS.html",
     indexT: "indexT.html",
     indexU: "indexU.html",
-    indexV: "indexV.html",    
-    indexW: "indexW.html",    
+    indexV: "indexV.html",
+    indexW: "indexW.html",
+    indexX: "indexX.html",
+    indexY: "indexY.html",
 };
 
 Object.entries(buttonMap).forEach(([id, url]) => {
